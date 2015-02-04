a4c1152831cd44f8debd25c5e3f0c6990dbd3e32
Gabriele Perego <gabriele.perego@sncegroup.ch>
2015-02-04 12:01:05 +0100

    Script updated to support v1 DO API version

--- do.sh
+++ do.sh
@@ -1,5 +1,6 @@
 endpoints=(sizes regions images ssh_keys)
 for i in "${endpoints[@]}"; do
-  url=`printf "https://api.digitalocean.com/%s/?client_id=%s&api_key=%s" $i $DO_CLIENT_ID $DO_API_KEY`
+  url=`printf "https://api.digitalocean.com/v1/%s/?client_id=%s&api_key=%s" $i $DO_CLIENT_ID $DO_API_KEY`
+  echo $url
   curl $url | python -mjson.tool
 done
